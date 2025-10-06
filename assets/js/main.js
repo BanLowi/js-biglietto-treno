a691c1e6e3fc97583f4b15e786c9ada9036f5883
BanLowi <lowi40k@gmail.com>
2025-10-06 19:11:08 +0200

    ticket price conditions

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const ticket_default = 0.21;
+
+let user_age = prompt("Inserisci Età")
+let user_distance = prompt("Inserisci Distanza")
+
+let ticket_price;
+
+if (user_age >= 65) {
+    ticket_price = (ticket_default * user_distance) - (ticket_default * user_distance * 0.4);
+
+}
+else if (user_age < 18) {
+    ticket_price = (ticket_default * user_distance) - (ticket_default * user_distance * 0.2);
+}
+else {
+    ticket_price = ticket_default * user_distance;
+}
+console.log(ticket_price);
+
